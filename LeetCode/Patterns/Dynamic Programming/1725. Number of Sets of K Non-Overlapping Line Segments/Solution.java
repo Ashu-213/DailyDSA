8eab4081886d2ad6ba683b1c091dc5716c441961
Ashutosh patidar <ashuptdr4@gmail.com>
2026-09-16 21:11:13 +0530

    Add Medium Java Solution for 1725. Number of Sets of K Non-Overlapping Line Segments from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/1725. Number of Sets of K Non-Overlapping Line Segments/Solution.java
@@ -0,0 +1,29 @@
+class Solution {
+    private static final int MOD = 1_000_000_007;
+
+    public int numberOfSets(int n, int k) {
+        int N = n + k - 1;
+        int R = 2 * k;
+
+        if (R > N - R) {
+            R = N - R;
+        }
+
+        long res = 1;
+        for (int i = 1; i <= R; i++) {
+            res = res * (N - R + i) % MOD;
+            res = res * modPow(i, MOD - 2) % MOD;
+        }
+        return (int) res;
+    }
+    private long modPow(long base, long exp) {
+        long result = 1;
+        base %= MOD;
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = result * base % MOD;
+            base = base * base % MOD;
+            exp >>= 1;
+        }
+        return result;
+    }
+}
